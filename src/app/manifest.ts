f7ac9b5eccaa4574a6cff8ba600d9f478552b51c
Matija Culjak <matija.culjak@gmail.com>
2026-03-04 09:10:30 +0900

    chore(pwa): generate additional icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -23,19 +23,18 @@ export default function manifest(): MetadataRoute.Manifest {
 				type: 'image/svg+xml',
 				purpose: 'any',
 			},
-			// Add 192x192 and 512x512 PNG for better support (e.g. public/icon-192.png, icon-512.png)
-			// {
-			//   src: '/icon-192.png',
-			//   sizes: '192x192',
-			//   type: 'image/png',
-			//   purpose: 'any',
-			// },
-			// {
-			//   src: '/icon-512.png',
-			//   sizes: '512x512',
-			//   type: 'image/png',
-			//   purpose: 'any maskable',
-			// },
+			{
+				src: '/icon-192.png',
+				sizes: '192x192',
+				type: 'image/png',
+				purpose: 'any',
+			},
+			{
+				src: '/icon-512.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'any maskable',
+			},
 		],
 		categories: ['navigation', 'travel'],
 		lang: 'en',
