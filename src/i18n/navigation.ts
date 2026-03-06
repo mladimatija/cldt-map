2b955cea3996650b098b487833c03fea2b1294db
Matija Culjak <matija.culjak@gmail.com>
2026-03-06 20:17:03 +0900

    refactor(footer): use Link for language switcher instead of Button

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,5 +1,5 @@
-/** Locale-aware Link and useRouter for next-intl (used instead of next/link and next/navigation in app). */
+/** Locale-aware Link, useRouter, usePathname for next-intl (used instead of next/link and next/navigation in app). */
 import { createNavigation } from 'next-intl/navigation';
 import { routing } from './routing';
 
-export const { Link, useRouter } = createNavigation(routing);
+export const { Link, useRouter, usePathname } = createNavigation(routing);
